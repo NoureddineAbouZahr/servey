fea005ebbb42b6d24aeb1ec905f079da1a1283c8
nour az <naz.199860@gmail.com>
2022-06-19 04:14:06 +0300

    creating questions controller with get and add functions

---
+++ jwt/app/Http/Controllers/quchoiceController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class quchoiceController extends Controller
+{
+    //
+}
--- jwt/app/Http/Controllers/questionController.php
+++ jwt/app/Http/Controllers/questionController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Question;
 
 class questionController extends Controller
 {
-    //
+
+    public function getAllQuestions($id = null){
+        if($id != null){
+            $questions = Question::find($id);
+        }else{
+            $questions = Question::all();
+        }
+        
+        return response()->json([
+            "status" => "Success",
+            "questions" => $questions
+        ], 200);
+    }
+
+
+
+
+    public function addQuestion(Request $request){
+        $question = new Question;
+        $question->name = $request->name;
+        
+        $question->type_id = $request->type_id;
+        $question->save();
+        
+        return response()->json([
+            "status" => "Success"
+        ], 200);
+    }
 }
